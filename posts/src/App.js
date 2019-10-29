7ad06edc3e6295b4e79a8fbfeb48f5cfdf71fef3
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-29 15:08:46 +0000

    Stretch: Refactored old code to manage posts and comments

--- posts/src/App.js
+++ posts/src/App.js
@@ -1,89 +1,102 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const initialUserFormState = {
-	name: "",
-	bio: "",
+const initialPostFormState = {
+  title: "",
+  contents: "",
 }
 function App() {
-	const [users, setUsers] = useState([]);
-	const [userForm, setUserForm] = useState(initialUserFormState);
-	const [isEditingUser, setEditingUser] = useState(0);
+  const [posts, setPosts] = useState([]);
+  const [postForm, setPostForm] = useState(initialPostFormState);
+  const [isEditingPost, setEditingPost] = useState(0);
 
-	useEffect(() => {
-		axios.get('http://localhost:4000/api/users').then(response => {
-			setUsers(response.data);
-		}).catch(err => console.log(err));
-	}, []);
-	
-	const onUserInputChange = e => {
-		setUserForm({ ...userForm, [e.target.name]: e.target.value });
-	}
+  useEffect(() => {
+    axios.get('http://localhost:4000/api/posts').then(response => {
+      console.log('get', response.data);
+      setPosts(response.data);
+    }).catch(err => console.log(err));
+  }, []);
 
-	const onUserSubmit = e => {
-		e.preventDefault();
-		if (isEditingUser === 0) {
-			axios.post('http://localhost:4000/api/users', userForm).then(response => {
-				setUsers([...users, { ...userForm, id: response.data.id }]);
-			}).catch(err => console.log(err));
-		} else {
-			axios.put('http://localhost:4000/api/users/' + isEditingUser, userForm).then(response => {
-				setUsers(users.map(user => {
-					if (Number(user.id) === isEditingUser) return { ...user, ...userForm }
-					return user;
-				}));
-			}).catch(err => console.log(err));
-		}
-		setUserForm(initialUserFormState);
-		setEditingUser(0);
-	}
+  const onPostInputChange = e => {
+    setPostForm({ ...postForm, [e.target.name]: e.target.value });
+  }
 
-	const removeUser = id => e => {
-		axios.delete('http://localhost:4000/api/users/' + id).then(response => {
-			setUsers(users.filter(user => Number(user.id) !== Number(id)));
-		}).catch(err => console.log(err));
-	}
+  const onPostSubmit = e => {
+    e.preventDefault();
+    if (isEditingPost === 0) {
+      axios.post('http://localhost:4000/api/posts', postForm).then(response => {
+        console.log('post', response.data);
+        setPosts([...posts, { ...postForm, id: response.data.id }]);
+      }).catch(err => console.log(err));
+    } else {
+      axios.put('http://localhost:4000/api/posts/' + isEditingPost, postForm).then(response => {
+        console.log('put', response.data);
+        setPosts(posts.map(post => {
+          if (Number(post.id) === isEditingPost) return response.data;
+          return post;
+        }));
+      }).catch(err => console.log(err));
+    }
+    setPostForm(initialPostFormState);
+    setEditingPost(0);
+  }
 
-	const editUser = id => e => {
-		setEditingUser(id);
-		setUserForm(users.find(user => Number(user.id) === Number(id)))
-	}
+  const removePost = id => e => {
+    axios.delete('http://localhost:4000/api/posts/' + id).then(response => {
+      console.log('delete', response.data);
+      debugger;
+      setPosts(posts.filter(post => Number(post.id) !== Number(response.data.id)));
+    }).catch(err => console.log(err));
+  }
 
-	return (
-		<div className="App">
-			{
-				users ? users.map(user => (
-					<div key={user.id} className="user">
-						<p>ID: {user.id}</p>
-						<p>Name: {user.name}</p>
-						<p>Bio: {user.bio}</p>
-						<button className="removeUser" onClick={removeUser(user.id)}>Remove</button>
-						<button className="editUser" onClick={editUser(user.id)}>Edit</button>
-					</div>
-				)) : null
-			}
-			<br />
-			<form onSubmit={onUserSubmit}>
-				<input
-					type="text"
-					name="name"
-					placeholder="User name here"
-					required
-					value={userForm.name}
-					onChange={onUserInputChange}
-				/>
-				<input
-					type="text"
-					name="bio"
-					placeholder="User bio here"
-					required
-					value={userForm.bio}
-					onChange={onUserInputChange}
-				/>
-				<button className="submitUser">Submit</button>
-			</form>
-		</div>
-	);
+  const editPost = id => e => {
+    setEditingPost(id);
+    setPostForm(posts.find(post => Number(post.id) === Number(id)))
+  }
+
+  return (
+    <div className="App">
+      {
+        posts ? posts.map(post => (
+          <div key={post.id} className="post">
+            <h2>Post #{post.id}</h2>
+            <p><b>Title</b>: {post.title}</p>
+            <p><b>Contents</b>: {post.contents}</p>
+            <h3>Comments</h3>
+            {
+              post.comments.length ? post.comments.map(comment => (
+                <div key={comment.id} className="comment">
+                  <p><b>#{comment.id}</b>: {comment.text}</p>
+                </div>
+              )) : <p>No comments!</p>
+            }
+            <button className="editPost" onClick={editPost(post.id)}>Edit Post</button>
+            <button className="removePost" onClick={removePost(post.id)}>Remove Post</button>
+          </div>
+        )) : null
+      }
+      <br />
+      <form onSubmit={onPostSubmit}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Post title here"
+          required
+          value={postForm.title}
+          onChange={onPostInputChange}
+        />
+        <input
+          type="text"
+          name="contents"
+          placeholder="Post contents here"
+          required
+          value={postForm.contents}
+          onChange={onPostInputChange}
+        />
+        <button className="submitPost">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;

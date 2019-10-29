d0669fba4b0f6aabec2121a92a223db6e8b658ab
Melquisedeque Pereira <melqui1@live.com.pt>
2019-10-29 13:37:37 +0000

    Added post endpoints with calls to database

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -5,12 +5,48 @@ const router = express.Router();
 
 // Creates a post using the information sent inside the request body.
 router.post('/', (req, res) => {
-  const post = req.body;
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({ errorMessage: "Please provide title and contents for the post." }).end();
+  }
+  db.insert({ title, contents }).then(data => {
+    db.findById(data.id).then(data => {
+      res.status(201).json(data);
+    }).catch(err => {
+      res.status(500).json({ errorMessage: "Could not get newly created post." }).end();
+    });
+  }).catch(err => {
+    res.status(500).json({ error: "There was an error while saving the post to the database" }).end();
+  });
 });
 
 // Creates a comment for the post with the specified id using information sent inside of the request body.
 router.post('/:id/comments', (req, res) => {
-  const post = req.body;
+  const { id } = req.params;
+  const { text } = req.body;
+  if (!text) {
+    res.status(400).json({ errorMessage: "Please provide text for the comment." }).end();
+  }
+  // find user
+  db.findById(id).then(data => {
+    if (!data) {
+      res.status(404).json({ message: "The post with the specified ID does not exist." });
+    } else {
+      // add comment
+      db.insertComment({ text }).then(comment => {
+        // get comment data
+        db.findCommentById(comment.id).then(newComment => {
+          res.status(201).json(newComment);
+        }).catch(err => {
+          res.status(500).json({ errorMessage: "Could not get newly created comment." }).end();
+        });
+      }).catch(err => {
+        res.status(500).json({ error: "There was an error while saving the comment to the database" }).end();
+      });
+    }
+  }).catch(err => {
+    res.status(500).json({ errorMessage: "Could not find user by id." }).end();
+  });
 });
 
 // Returns an array of all the post objects contained in the database.
@@ -83,3 +119,5 @@ router.put('/:id', (req, res) => {
     res.status(500).json().end();
   });
 });
+
+module.exports = router;
